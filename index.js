231b12e09663a9b268e0612a38336f0b649f8905
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-08-24 14:10:23 -0700

    /address

--- index.js
+++ index.js
@@ -20,6 +20,23 @@ app.get('/getethprice', async (req, res) => {
   }
 });
 
+app.get('/address', async (req, res) => {
+  try {
+    const { query } = req;
+    const chain = '0x1';
+    const response =
+      await Moralis.EvmApi.transaction.getWalletTransactionsVerbose({
+        address: query,
+        address,
+        chain,
+      });
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(`Something went wrong: ${e}`);
+    return res.status(400).json();
+  }
+});
+
 Moralis.start({
   apiKey: process.env.MORALIS_KEY,
 }).then(() => {
